02dfaf9b3979467b1af70f52fb8c337bc3630ad3
ypei23 <ypei23@cmc.edu>
2023-01-28 14:46:07 -0800

    again

--- Fixme.py
+++ Fixme.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''
 Complete each function below so that the test cases pass.
-Your solutions should use the map and filter functions,
+Your solutions should use the map functions,
 and not for loops or list comprehensions.
 '''
 
